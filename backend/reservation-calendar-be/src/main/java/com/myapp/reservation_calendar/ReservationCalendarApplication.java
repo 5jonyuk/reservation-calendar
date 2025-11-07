f0caa8839e650482be9844b4c98a1861f62d0e24
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-07 14:31:26 +0900

    feat: 프로젝트 생성 후 첫 커밋

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/ReservationCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.reservation_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationCalendarApplication.class, args);
+	}
+
+}
---
+++ backend/reservation-calendar-be/src/test/java/com/myapp/reservation_calendar/ReservationCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.reservation_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
