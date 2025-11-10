5541890efecebbda6c0693781f8e88683f855a04
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-10 10:43:45 +0900

    feat: 예약 엔티티 추가

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/reservation/Reservation.java
@@ -0,0 +1,52 @@
+package com.myapp.reservation_calendar.reservation;
+
+import com.myapp.reservation_calendar.reservation.dto.ReservationRequest;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private String customerPhone;
+
+    @Column(nullable = false)
+    private String menu;
+
+    @Column(nullable = false)
+    private LocalDateTime pickupTime;
+
+    private LocalDateTime dropTime;
+    private boolean paymentCompleted;
+    private boolean pickupCompleted;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastUpdatedAt;
+
+    public static Reservation from(ReservationRequest request) {
+        return Reservation.builder()
+                .customerName(request.customerName())
+                .customerPhone(request.customerPhone())
+                .menu(request.menu())
+                .pickupTime(request.pickupTime())
+                .dropTime(request.dropTime())
+                .paymentCompleted(request.paymentCompleted())
+                .pickupCompleted(request.pickupCompleted())
+                .build();
+    }
+}
