3f94d25961cd413517eae9e39e83280f7f52b0e3
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-10 11:08:57 +0900

    feat: 예약 생성 및 검증 후 레포지토리로 넘기는 서비스코드 생성

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/reservation/ReservationService.java
@@ -0,0 +1,20 @@
+package com.myapp.reservation_calendar.reservation;
+
+import com.myapp.reservation_calendar.reservation.dto.ReservationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+    private final ReservationJpaRepository reservationJpaRepository;
+    private final ReservationValidator reservationValidator;
+
+    @Transactional
+    public Reservation createReservation(ReservationRequest request){
+        Reservation reservation = Reservation.from(request);
+        reservationValidator.validate(reservation);
+        return reservationJpaRepository.save(reservation);
+    }
+}
