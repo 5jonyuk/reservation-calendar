4227fb5f195769d6d96f9fd71314537e8a1029e5
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-10 14:55:55 +0900

    chore: 테스트에 사용할 mock의존성 추가 및 app 코드포맷팅

--- backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/ReservationCalendarApplication.java
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/ReservationCalendarApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReservationCalendarApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationCalendarApplication.class, args);
 	}
-
 }
