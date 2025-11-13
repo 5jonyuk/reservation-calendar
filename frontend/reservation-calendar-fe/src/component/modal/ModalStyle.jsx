785feba86ddce5ad730babf25c690c88c086e114
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-14 01:39:18 +0900

    refactor: 모달 스타일 코드 컴포넌트화

---
+++ frontend/reservation-calendar-fe/src/component/modal/ModalStyle.jsx
@@ -0,0 +1,12 @@
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    backdropFilter: "blur(4px)",
+    WebkitBackdropFilter: "blur(4px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 50,
+  },
+};
+export default customStyles;
