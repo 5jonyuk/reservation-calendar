cb95ce329651d3d395a074efd8f81d66005ff4bb
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-14 23:46:58 +0900

    feat(frontend): 파비콘 추가 및 apiUrl 배포url로 변경

--- frontend/reservation-calendar-fe/src/config/ApiUrl.jsx
+++ frontend/reservation-calendar-fe/src/config/ApiUrl.jsx
@@ -1,5 +1,7 @@
-const dev = true; //배포 시 반드시 false로 변경
+const dev = false; //배포 시 반드시 false로 변경
 
-const apiUrl = dev ? "http://localhost:8080" : "https://api.example.com";
+const apiUrl = dev
+  ? "http://localhost:8080"
+  : "https://yuri-snack-house.vercel.app";
 
 export default apiUrl;
