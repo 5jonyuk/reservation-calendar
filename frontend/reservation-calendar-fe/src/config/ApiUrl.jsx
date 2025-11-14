152b671104e326822289df87c7dd2c65bfe261bb
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-15 00:00:04 +0900

    fix(frontend): 배포 url 프론트url이 아닌 서버 url로 수정

--- frontend/reservation-calendar-fe/src/config/ApiUrl.jsx
+++ frontend/reservation-calendar-fe/src/config/ApiUrl.jsx
@@ -2,6 +2,6 @@ const dev = false; //배포 시 반드시 false로 변경
 
 const apiUrl = dev
   ? "http://localhost:8080"
-  : "https://yuri-snack-house.vercel.app";
+  : "https://reservation-calendar-ek60.onrender.com";
 
 export default apiUrl;
