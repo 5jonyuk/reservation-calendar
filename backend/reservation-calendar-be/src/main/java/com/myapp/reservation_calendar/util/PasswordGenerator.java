c6c8ab7bea040319af7543817e8a759d01e0e266
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-18 22:26:30 +0900

    feat: 암호화된 패스워트 생성기 추가
    - 회원가입 기능을 만들지 않을 것이기 때문에
    DB에 패스워드를 암호화하여 저장하기위해 추가
    - rawPassword는 도커컴포즈에서 환경변수로 가져옴

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/util/PasswordGenerator.java
@@ -0,0 +1,19 @@
+package com.myapp.reservation_calendar.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordGenerator implements CommandLineRunner {
+    @Value("${raw_pwd}")
+    private String rawPassword;
+
+    @Override
+    public void run(String... args) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encoded = encoder.encode(rawPassword);
+        System.out.println("Encoded password: " + encoded);
+    }
+}
