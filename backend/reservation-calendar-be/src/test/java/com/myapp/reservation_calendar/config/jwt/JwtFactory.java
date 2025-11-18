db0504b3d9783cc97afa96a33dd9e12e1ade3f67
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-19 02:10:36 +0900

    tset: jwt 토큰서비스를 테스트할 모킹용(가짜) 객체 추가
    - Getter를 사용하기 위한 테스트 롬복 의존성 추가

---
+++ backend/reservation-calendar-be/src/test/java/com/myapp/reservation_calendar/config/jwt/JwtFactory.java
@@ -0,0 +1,49 @@
+package com.myapp.reservation_calendar.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+public class JwtFactory {
+    private String subject = "admin";
+    private Date issuedAt = new Date();
+    private Date expiration = new Date(issuedAt.getTime() + Duration.ofDays(14).toMillis());
+    private Map<String, Object> claims = Collections.emptyMap();
+    SecretKey key = Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
+
+    @Builder
+    public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
+        this.subject = subject != null ? subject : this.subject;
+        this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
+        this.expiration = expiration != null ? expiration : this.expiration;
+        this.claims = claims != null ? claims : this.claims;
+    }
+
+    public static JwtFactory withDefaultValues() {
+        return JwtFactory.builder().build();
+    }
+
+    public String createToken(JwtProperties jwtProperties) {
+        return Jwts.builder()
+                .header()
+                .type("JWT")
+                .and()
+
+                .subject(subject)
+                .issuedAt(issuedAt)
+                .expiration(expiration)
+                .claims(claims)
+
+                .signWith(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8)), Jwts.SIG.HS256)
+                .compact();
+    }
+}
