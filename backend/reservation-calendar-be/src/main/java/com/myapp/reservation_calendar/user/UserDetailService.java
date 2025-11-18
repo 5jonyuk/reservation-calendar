93f8ff8229904c07a10d2e83917ce5b901d87b43
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-18 19:24:50 +0900

    feat: 시큐리티에서 로그인을 진행할 때 사용자 정보를 가져오는 기능 추가

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/user/UserDetailService.java
@@ -0,0 +1,17 @@
+package com.myapp.reservation_calendar.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+    }
+}
