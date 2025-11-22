ef54fadd6baf36b240fed9865003025b88a62883
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-23 00:40:16 +0900

    feat: 로그인 api 추가

---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/login/LoginController.java
@@ -0,0 +1,36 @@
+package com.myapp.reservation_calendar.login;
+
+import com.myapp.reservation_calendar.config.jwt.TokenProvider;
+import com.myapp.reservation_calendar.login.dto.LoginRequestDTO;
+import com.myapp.reservation_calendar.user.User;
+import com.myapp.reservation_calendar.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+    private static final Duration ACCESS_TOKEN_EXPIRED_AT = Duration.ofMinutes(30);
+    private final UserService userService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequest) {
+        try {
+            User user = userService.authenticate(loginRequest.username(), loginRequest.password());
+            String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_EXPIRED_AT);
+            return ResponseEntity.ok(accessToken);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("서버 오류가 발생했습니다.");
+        }
+    }
+}
---
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/login/dto/LoginRequestDTO.java
@@ -0,0 +1,7 @@
+package com.myapp.reservation_calendar.login.dto;
+
+public record LoginRequestDTO(
+        String username,
+        String password
+) {
+}
