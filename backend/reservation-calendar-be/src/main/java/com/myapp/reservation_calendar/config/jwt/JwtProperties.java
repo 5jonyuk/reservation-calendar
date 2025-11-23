0789cc250dd40276847c6fae803aedb8c923b938
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-23 14:22:04 +0900

    feat: 액세스 토큰과 리프레시 토큰의 유효기간 추가

--- backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/config/jwt/JwtProperties.java
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/config/jwt/JwtProperties.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 @Getter
 @Setter
 @Component
@@ -12,4 +14,6 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
     private String issuer;
     private String secretKey;
+    private Duration accessTokenExpiration = Duration.ofMinutes(30);
+    private Duration refreshTokenExpiration = Duration.ofDays(14);
 }
