762280dda67fdbc7c1fbcd83709f568e5036603a
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-23 14:28:41 +0900

    feat: 리프레시토큰을 생성하여 DB에 저장하고 응답을 반환하는 로직 추가

--- backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/login/LoginController.java
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/login/LoginController.java
@@ -1,7 +1,10 @@
 package com.myapp.reservation_calendar.login;
 
 import com.myapp.reservation_calendar.config.jwt.TokenProvider;
-import com.myapp.reservation_calendar.login.dto.LoginRequestDTO;
+import com.myapp.reservation_calendar.login.dto.LoginRequest;
+import com.myapp.reservation_calendar.login.dto.LoginResponse;
+import com.myapp.reservation_calendar.refreshToken.RefreshToken;
+import com.myapp.reservation_calendar.refreshToken.RefreshTokenService;
 import com.myapp.reservation_calendar.user.User;
 import com.myapp.reservation_calendar.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +22,23 @@ import java.time.Duration;
 public class LoginController {
     private static final Duration ACCESS_TOKEN_EXPIRED_AT = Duration.ofMinutes(30);
     private static final String ERROR_MESSAGE_500 = "[ERROR] 서버 오류가 발생했습니다.";
+
     private final UserService userService;
     private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequest) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
             User user = userService.authenticate(loginRequest.username(), loginRequest.password());
             String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_EXPIRED_AT);
-            return ResponseEntity.ok(accessToken);
+            String refreshToken = tokenProvider.generateRefreshToken(user);
+
+            refreshTokenService.saveOrUpdate(user.getId(), refreshToken);
+
+            LoginResponse responseDTO = new LoginResponse(accessToken, refreshToken);
+            return ResponseEntity.ok(responseDTO);
+
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(401).body(e.getMessage());
         } catch (Exception e) {
