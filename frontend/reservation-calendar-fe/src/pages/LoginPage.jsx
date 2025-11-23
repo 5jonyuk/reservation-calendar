dd089a3e94dd46a30019167b4c9247a41cc0d673
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-24 00:03:42 +0900

    refactor(frontend): Icon 컴포넌트 호출하여 코드 중복 제거

--- frontend/reservation-calendar-fe/src/pages/LoginPage.jsx
+++ frontend/reservation-calendar-fe/src/pages/LoginPage.jsx
@@ -1,21 +1,7 @@
 import React, { useState } from "react";
 import apiUrl from "../config/ApiUrl";
 import logo from "../img/logo.png";
-
-const Icon = ({ children, className = "w-5 h-5", viewBox = "0 0 24 24" }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox={viewBox}
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className={className}
-  >
-    {children}
-  </svg>
-);
+import Icon from "../component/Icon";
 
 const UserIcon = (props) => (
   <Icon {...props}>
@@ -28,14 +14,6 @@ const LockIcon = (props) => (
   <Icon {...props}>
     <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
     <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-  </Icon>
-);
-
-const LogInIcon = (props) => (
-  <Icon {...props}>
-    <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-    <polyline points="10 17 15 12 10 7" />
-    <line x1="15" x2="3" y1="12" y2="12" />
   </Icon>
 );
 
