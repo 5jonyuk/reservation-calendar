f1546b9f28c537259547c1ab17dbc08483e53019
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-24 10:11:33 +0900

    refactor: http 응답 코드를 유용하게 사용하기 위해 변경

--- backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/reservation/ReservationController.java
+++ backend/reservation-calendar-be/src/main/java/com/myapp/reservation_calendar/reservation/ReservationController.java
@@ -1,8 +1,10 @@
 package com.myapp.reservation_calendar.reservation;
 
+import com.myapp.reservation_calendar.ApiResponse;
 import com.myapp.reservation_calendar.reservation.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -13,12 +15,22 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/reservation")
 public class ReservationController {
+    private static final String ERR_MESSAGE_SERVER_ERROR = "[ERROR] 서버오류가 발생했습니다. 관리자에게 문의해주세요.";
+
     private final ReservationService reservationService;
 
     @PostMapping
-    public ReservationCreateResponse createReservation(@RequestBody ReservationCreateRequest request) {
-        Reservation savedReservation = reservationService.createReservation(request);
-        return ReservationMapper.toCreateResponse(savedReservation);
+    public ResponseEntity<ApiResponse<?>> createReservation(@RequestBody ReservationCreateRequest request) {
+        try {
+            Reservation savedReservation = reservationService.createReservation(request);
+            ReservationCreateResponse response = ReservationMapper.toCreateResponse(savedReservation);
+
+            return ResponseEntity.ok(ApiResponse.success(response, null));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(ApiResponse.error(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(ApiResponse.error(ERR_MESSAGE_SERVER_ERROR));
+        }
     }
 
     @GetMapping("/month")
@@ -39,18 +51,34 @@ public class ReservationController {
     }
 
     @PatchMapping("/{id}")
-    public ReservationUpdateResponse updateReservation(
+    public ResponseEntity<ApiResponse<?>> updateReservation(
             @PathVariable Long id,
             @RequestBody ReservationUpdateRequest request) {
-        Reservation updatedReservation = reservationService.updateReservation(id, request);
-        return ReservationMapper.toUpdateResponse(updatedReservation);
+        try {
+            Reservation updatedReservation = reservationService.updateReservation(id, request);
+            ReservationUpdateResponse response = ReservationMapper.toUpdateResponse(updatedReservation);
+
+            return ResponseEntity.ok(ApiResponse.success(response, null));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(ApiResponse.error(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(ApiResponse.error(ERR_MESSAGE_SERVER_ERROR));
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ReservationDeleteResponse deleteReservation(
+    public ResponseEntity<ApiResponse<?>> deleteReservation(
             @PathVariable Long id
-    ){
-        reservationService.deleteReservation(id);
-        return ReservationMapper.toDeleteResponse(id);
+    ) {
+        try {
+            reservationService.deleteReservation(id);
+            ReservationDeleteResponse response = ReservationMapper.toDeleteResponse(id);
+
+            return ResponseEntity.ok(ApiResponse.success(response, null));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(ApiResponse.error(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(ApiResponse.error(ERR_MESSAGE_SERVER_ERROR));
+        }
     }
 }
